cf44415c2d122c0d20923bc1aebd27b5401c1e6c
Vladimir Peresadin <107420917+vladperesad@users.noreply.github.com>
2023-06-01 16:28:36 +0800

    code for cleaning the rest of the dataset

--- sh_car_market.sql
+++ sh_car_market.sql
@@ -284,21 +284,202 @@ DELETE FROM
 WHERE
 	power <10;
     
-# let's turn columns id, price, mileage to INT type as well   
+# let's turn columns id, price, mileage, reg_year, reg_month to INT type as well   
 
 ALTER TABLE autos_cleaned
 MODIFY id INT,
 MODIFY price INT,
-MODIFY mileage INT;
+MODIFY mileage INT,
+MODIFY reg_year INT,
+MODIFY reg_mon INT;
 
+#take a look at the price range
 SELECT
 	MAX(price),
     MIN(price)
 FROM
 	autos_cleaned;
     
-	
+# remove records with 0 
+DELETE FROM
+	autos_cleaned
+WHERE
+	price = 0;
+    
+#in terms of upper limit, models and prices above 745000 don't make much sense (numbers like 911911, 123456 or 999999)
+    
+
+DELETE FROM
+	autos_cleaned
+WHERE
+	price > 745000;
+    
+SELECT
+	*
+FROM
+	autos_cleaned
+WHERE
+	price <1000
+ORDER BY price;
+
+#prices below 1000 are probably connected with the condition of the car (totalled, seriously damaged etc)
+#get back to the prices when i'm done with the damage column
+
+SELECT
+	COUNT(id)
+FROM
+	autos_cleaned
+WHERE
+	damage = "";
+    
+#43383 rows with missing value for damage, but since it is an important information and there is no way 
+#i can derive it from other columns, i'll simply have to delete it 
+#prior to that turn ja and nein into true and false
 
 
+UPDATE autos_cleaned
+SET damage = '1'
+WHERE damage = 'TRUE';
 
-#---
+UPDATE autos_cleaned
+SET damage = '0'
+WHERE damage = 'FALSE';
+ 
+UPDATE autos_cleaned
+SET damage = NULL
+WHERE damage = '';
+ 
+ALTER TABLE autos_cleaned
+MODIFY damage TINYINT(1);
+
+DELETE FROM autos_cleaned
+WHERE damage IS NULL;
+
+SELECT
+	DISTINCT(damage)
+FROM
+	autos_cleaned;
+    
+#get back to prices
+
+DELETE FROM
+	autos_cleaned
+WHERE
+	price <100
+ORDER BY price;
+
+#seller
+
+SELECT
+	DISTINCT(seller)
+FROM
+	autos_cleaned;
+    
+UPDATE autos_cleaned
+SET seller = 'private'
+WHERE seller = 'privat';
+
+    
+UPDATE autos_cleaned
+SET seller = 'commercial'
+WHERE seller = 'gewerblich';
+
+#take a look at the fuel coulumn
+
+SELECT
+	DISTINCT(fuel)
+FROM
+	autos_cleaned;
+    
+UPDATE autos_cleaned 
+SET fuel = 'gasoline'
+WHERE fuel = 'benzin';
+
+UPDATE autos_cleaned
+SET fuel = 'other'
+WHERE fuel = 'andere';
+
+UPDATE autos_cleaned
+SET fuel = 'electric'
+WHERE fuel = 'elektro';
+
+#pull up min and max values in mileage column    
+SELECT
+	MIN(mileage),
+    MAX(mileage)
+FROM
+	autos_cleaned;
+#the min and max mileages seem to make sense
+
+#finally take a look at the year and month of registration
+
+SELECT
+	MAX(reg_year),
+    MIN(reg_year)
+FROM
+	autos_cleaned;
+#while 1910 is technically possible, anything after 2019 is not
+#clean up any value larger than 2019 and take a closer look at cars before 1950
+
+DELETE FROM autos_cleaned
+WHERE reg_year > 2019;
+
+SELECT
+	brand,
+    model,
+    vehicle_name,
+    power,
+    reg_year
+FROM
+	autos_cleaned
+WHERE
+	reg_year < 1930
+ORDER BY reg_year;
+
+#the only car that doesn't fit is a vw beetle with TDI angine that produces 90hp, not only because in 1910
+#there wasn't such a thing as TDI, and hp of 90 was undeard of for 1.9 litre engine but also because first beetle was made in 1965 
+
+DELETE FROM autos_cleaned
+WHERE reg_year <1923;
+
+#let's look at the months avaliable 
+
+SELECT
+	DISTINCT(reg_mon)
+FROM
+	autos_cleaned
+ORDER BY 
+	reg_mon;
+
+#get rid of the 0 months
+
+DELETE FROM
+	autos_cleaned
+WHERE
+	reg_mon = 0;
+    
+#create a new column and put concatenated year and month in there
+
+
+ALTER TABLE autos_cleaned
+ADD COLUMN reg_date VARCHAR(25) AFTER reg_mon;
+
+#turn the column into a date type
+
+UPDATE autos_cleaned
+SET reg_date = STR_TO_DATE(reg_date,'%Y-%c');
+
+#clean up unused columns    
+ALTER TABLE autos_cleaned
+DROP COLUMN reg_year,
+DROP COLUMN reg_mon;
+        
+ALTER TABLE autos_cleaned
+CHANGE COLUMN date_created_2 date_created DATE,
+CHANGE COLUMN date_crawled_2 date_crawled DATE,
+CHANGE COLUMN last_seen_2 last_seen DATE;
+
+SELECT
+	*
+FROM
+	autos_cleaned;
