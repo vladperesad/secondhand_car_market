352848e1b420a11989e9d1b9ce8db04332c4dabc
Vladimir Peresadin <107420917+vladperesad@users.noreply.github.com>
2023-06-14 15:16:56 +0800

    mercedes models sorted

--- models_reshaping.R
+++ models_reshaping.R
@@ -57,9 +57,11 @@ models_to_filter <- c("",
                      "serie_3",
                      "serie_1")
 
+
+#create a subset with rows that have these unclear/unknown values in the `model` column
 autos_subset <- filter(autos, model %in% models_to_filter)
 
-#create a new column with " " instead of spaces
+#create a new column with " " instead of underscores "_"
 
 autos_subset$name_new <- gsub("_", " ",autos_subset$name)
 
@@ -69,8 +71,35 @@ autos_subset_tidy <- autos_subset %>%
   unnest_tokens(output = names_word,
                 input = name_new)
 
+#get rid of the brand names
 autos_subset_tidy <- autos_subset_tidy %>% 
   filter(!names_word %in% brands)
 
 #now that we have column without brand names we can pull model names from it 
 
+autos_subset_tidy <- autos_subset_tidy %>% 
+  group_by(index) %>% 
+  slice(1)
+
+#mercedes tend to use letter and number to indicate model E 120, 
+#so unless I pull up first two words I am gonna get only a letter which isn't very informative
+#create a subset for mercedes only
+
+mercedes <- autos_subset_tidy %>% 
+  filter(brand == "mercedes_benz") %>% 
+  group_by(index) %>% 
+  slice(1,2)
+
+mercedes<- mercedes %>% 
+  group_by(index) %>% 
+  mutate(model_new = paste(names_word, collapse = " ")) %>% 
+  slice(1)
+
+#theres also a brand named "sonstige_autos" that in fact isn't a brand,
+#but rather a sign that the car is made in the USA
+
+sonstige<- autos_subset_tidy %>% 
+  filter(brand == "sonstige_autos")
+
+
+  
